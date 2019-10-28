e0e6bf3014125d725e3f0cc1da862289de16a0ed
cwervo <acwervo@gmail.com>
2019-10-27 23:13:16 -0400

    Replace parcel with all static version

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-# Parc3l
+# PrinterBoi WebGL
 
-This is a little demo project providing the simplest proof of concept for combining the following technologies:
-
-- [Three.js](https://threejs.org/)
-- [Rust](https://www.rust-lang.org)
-- [Web Assembly](http://webassembly.org/)
-
-This is all done using [Parcel JS](https://parceljs.org/).
-
-You can see the project up and running at [https://parc3l.glitch.me/](https://parc3l.glitch.me/)
+bill by [Muhammad Faizal Rahman Hakim](https://thenounproject.com/search/?q=printer&i=783564) from the Noun Project
---
+++ example-three-scene.js
@@ -0,0 +1,106 @@
+import PrinterBoi from 'https://unpkg.com/printerboi@0.0.1/index.js';
+
+const CANVAS_SCALE = 1.0
+function getWidth() {
+    return window.innerWidth * CANVAS_SCALE
+}
+function getHeight() {
+    return window.innerHeight * CANVAS_SCALE
+}
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, getWidth()  / getHeight(), 0.1, 1000 );
+let gltfLoader = new THREE.GLTFLoader();
+let trackball, gameboyModel;
+
+// Note 'preserveDrawingBuffer: true' below, this is important!
+// Can cause artifacts[1], but should be okay for now with relatively simple scenes.
+// TODO: Figure out how to do this for WebGL contexts without preserveDrawingBuffer turned on!
+// 1: https://stackoverflow.com/questions/45221542/html-save-webgl-canvas-as-image#comment77422119_45223017
+var renderer = new THREE.WebGLRenderer({
+    antialias: false,
+    transparent: false,
+    preserveDrawingBuffer: true,
+    powerPreference: 'low-power',
+    failIfMajorPerformanceCaveat: true
+});
+renderer.setSize( getWidth(), getHeight() );
+
+let printerButtonEl = addPrinterButton()
+document.body.appendChild( renderer.domElement );
+
+trackball = new Trackball(renderer.domElement, {startSpin: 0.01});
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x2EAFAC, wireframe: true } );
+
+var light = new THREE.PointLight( 0xFFFFFF, 1, 100 );
+light.position.set( 1, 1, 1 );
+scene.add( light );
+// Sets the GL clear color to ensure background is white
+scene.background = new THREE.Color(0xFFFFFF)
+
+// -------
+// Load a glTF resource
+gltfLoader.load(
+// resource URL
+    'gameboy.glb',
+    // called when the resource is loaded
+    function ( gltf ) {
+        gameboyModel = gltf.scene
+        scene.add( gltf.scene );
+        gltf.animations; // Array<THREE.AnimationClip>
+        gltf.scene; // THREE.Scene
+        gltf.scenes; // Array<THREE.Scene>
+        gltf.cameras; // Array<THREE.Camera>
+        gltf.asset; // Object
+    },
+    // called while loading is progressing
+    function ( xhr ) { console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' ); },
+    // called when loading has errors
+    function ( error ) { console.log( 'An error happened' ); }
+);
+
+camera.position.z = 4;
+
+var animate = function () {
+    requestAnimationFrame( animate );
+
+    const m = new THREE.Matrix4();
+    const mat = trackball.getMatrix();
+    m.fromArray(mat)
+    if (gameboyModel != undefined) {
+        gameboyModel.rotation.setFromRotationMatrix(m)
+    }
+
+    renderer.render( scene, camera );
+};
+
+animate();
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+    camera.aspect = getWidth() / getHeight();
+    camera.updateProjectionMatrix();
+    renderer.setSize( getWidth(), getHeight());
+}
+
+function addPrinterButton() {
+    let printerButton = document.createElement('button')
+    printerButton.classList.add('printerButton')
+    printerButton.innerHTML = `Hit me to print! <img src="printer.png" alt="printer image"/>`
+    document.body.appendChild(printerButton)
+    return printerButton
+}
+
+
+printerButtonEl.onclick = () => {
+    console.log('hey')
+    let domTexture = new THREE.Texture(renderer.domElement)
+    // window.domTexture = domTexture
+    // Need to figure out how to get this from WebGL rather than just THREE!!
+    let pb = new PrinterBoi(domTexture)
+    pb.printPopup()
+}
+
